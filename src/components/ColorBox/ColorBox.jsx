3ae7012246cd142a52ea54687f4559a8d4a7c2e0
rzounlome <rzounlome@harmonyhit.com>
2020-11-20 11:58:43 -0500

    add CopyToClipboard

--- src/components/ColorBox/ColorBox.jsx
+++ src/components/ColorBox/ColorBox.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import "./ColorBox.scss";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 class ColorBox extends Component {
   render() {
     const { background, name } = this.props;
     return (
-      <div style={{ background: background }} className="ColorBox">
-        <div className="copy-container">
-          <div className="box-content">
-            <span>{name}</span>
+      <CopyToClipboard text={background}>
+        <div style={{ background: background }} className="ColorBox">
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
+            <span className="see-more">See More</span>
           </div>
-          <button className="copy-button">Copy</button>
-          <span className="see-more">See More</span>
         </div>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
