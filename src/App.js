5ade8d67c41fe63c058e8580e33002341f7aba63
rzounlome <rzounlome@harmonyhit.com>
2020-12-01 11:21:24 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 import Palette from "./components/Palette/Palette";
 import { PaletteColors } from "./seedColors";
 import { generatePalette } from "./utils/colorHelpers";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Palette palette={generatePalette(PaletteColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette Goes Here</h1>} />
+        <Route exact path="/palette/:id" />
+      </Switch>
+
+      // <div className="App">
+      //   <Palette palette={generatePalette(PaletteColors[4])} />
+      // </div>
     );
   }
 }
